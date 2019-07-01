b3789ebc2f5d8c081dc622500dfcdba7514d9293
lihuangit <472103722@qq.com>
2019-07-01 18:25:47 +0800

    李 Initial commit

---
+++ src/com/kgc/k9501/login.java
@@ -0,0 +1,13 @@
+package com.kgc.k9501;
+
+/**
+ * @program: untitled
+ * @ClassName: login
+ * @author: Mr.Li
+ * @create: 2019-07-01 18:24
+ **/
+public class login {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}
