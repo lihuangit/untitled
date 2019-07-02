52ec53b6a7dc699a4df30363336d3b4aa48af87d
lihuangit <472103722@qq.com>
2019-07-02 14:56:09 +0800

    小李提交的代码

---
+++ src/com/kgc/k9501/aaaa.java
@@ -0,0 +1,13 @@
+package com.kgc.k9501;
+
+/**
+ * @program: untitled
+ * @ClassName: aaaa
+ * @author: Mr.Li
+ * @create: 2019-07-02 14:55
+ **/
+public class aaaa {
+    public void aaa(){
+        System.out.println("hhah");
+    }
+}
